9df51a38aee2e99dbb6318a0319597cb81a2fc89
TANIGUCHI Masaya <ta2gch@gmail.com>
2025-12-27 17:26:14 +0900

    wip

--- Mathling/Lambek/ProductFree/Decidable.lean
+++ Mathling/Lambek/ProductFree/Decidable.lean
@@ -1,5 +1,6 @@
 import Mathlib.Data.Bool.Basic
 import Mathling.Lambek.ProductFree.Basic
+import Lean.LibrarySuggestions.Default
 
 namespace Mathling.Lambek.ProductFree
 
@@ -9,19 +10,19 @@ def splits {α} : List α → List (List α × List α)
   | x :: xs => ([], x :: xs) :: (splits xs).map (fun (l, r) => (x :: l, r))
 
 @[grind .]
-lemma splits_list_degree :
-  ∀ X ∈ splits Γ, (list_degree X.1) + (list_degree X.2) = (list_degree Γ) := by
-    induction Γ <;> grind
+lemma splits_list_degree (h : X ∈ splits Γ) :
+  X.1 ++ X.2 = Γ := by
+  induction Γ generalizing X <;> grind
 
 @[grind]
 def picks {α} : List α → List (List α × α × List α)
   | [] => []
   | x :: xs => ([], x, xs) :: (picks xs).map (fun (l, a, r) => (x :: l, a, r))
 
-@[grind .]
-lemma picks_list_degree :
-  ∀ X ∈ picks Γ, (list_degree X.1) + (tp_degree X.2.1) + (list_degree X.2.2) = (list_degree Γ) := by
-    induction Γ <;> grind
+@[grind =>]
+lemma picks_list_degree (h : X ∈ picks Γ) :
+   X.1 ++ [X.2.1] ++ X.2.2 = Γ := by
+  induction Γ generalizing X <;> grind
 
 @[grind]
 inductive Cand where
@@ -34,53 +35,56 @@ def candidates (Γ : List Tp) : List Cand :=
     match f with
     | B ⧸ A => (splits R).map (fun (Δ, Λ) => .rdiv L B A Δ Λ)
     | A ⧹ B => (splits L).map (fun (Γ, Δ) => .ldiv Γ A B Δ R)
-    | _ => [])
+    | # _ => [])
 
-@[grind .]
-lemma candidates_list_degree :
-  ∀ c ∈ candidates Γ,
+@[grind =>]
+lemma candidates_list_degree (h : c ∈ candidates Γ) :
   match c with
-    | .rdiv L B _ Δ Λ =>
-        list_degree Δ < list_degree Γ ∧
-        (list_degree L + tp_degree B + list_degree Λ) < list_degree Γ
-    | .ldiv Γ₀ _ B Δ R =>
-        list_degree Δ < list_degree Γ ∧
-        (list_degree Γ₀ + tp_degree B + list_degree R) < list_degree Γ := by
-  intro c hc
-  simp only [candidates, List.mem_flatMap, Prod.exists] at hc
-  rcases hc with ⟨L, f, R, hpicks, hf⟩
-  have h_f_deg := picks_list_degree (L, f, R) hpicks
-  cases f with
-  | rdiv B A =>
-    simp only [List.mem_map, Prod.exists] at hf
-    rcases hf with ⟨Δ, Λ, hsplit, rfl⟩
-    have h_split_deg := splits_list_degree (Δ, Λ) hsplit
-    grind
-  | ldiv A B =>
-    simp only [List.mem_map, Prod.exists] at hf
-    rcases hf with ⟨Γ₀, Δ, hsplit, rfl⟩
-    have h_split_deg := splits_list_degree (Γ₀, Δ) hsplit
-    grind
-  | _ => contradiction
+    | .rdiv L B A Δ Λ => L ++ [B ⧸ A] ++ Δ ++ Λ = Γ
+    | .ldiv Λ A B Δ R => Λ ++ [A ⧹ B] ++ Δ ++ R = Γ := by
+  simp only [candidates, List.mem_flatMap, Prod.exists] at h
+  rcases h with ⟨_, f, _, h_pick, h_cand⟩
+  have h_geom := picks_list_degree h_pick
+  match f with
+  | B ⧸ A =>
+      simp only [List.mem_map, Prod.exists] at h_cand
+      rcases h_cand with ⟨_, _, h_split, _⟩
+      have h_geom_split := splits_list_degree h_split
+      sorry
+  | A ⧹ B =>
+      simp only [List.mem_map, Prod.exists] at h_cand
+      rcases h_cand with ⟨_, _, h_split, _⟩
+      have h_geom_split := splits_list_degree h_split
+      sorry
+  | # _ => grind
 
 def prove (Γ : List Tp) (A : Tp) : Bool :=
   match A with
   | Tp.atom s =>
     (Γ = [Tp.atom s]) ||
     (candidates Γ).any (fun
-      | .rdiv L B A Δ Λ =>
-          prove Δ A && prove (L ++ [B] ++ Λ) (Tp.atom s)
-      | .ldiv Γ A B Δ R =>
-          prove Δ A && prove (Γ ++ [B] ++ R) (Tp.atom s))
-  | Tp.ldiv A B =>
-    Γ ≠ [] && prove ([A] ++ Γ) B
-  | Tp.rdiv B A =>
-    Γ ≠ [] && prove (Γ ++ [A]) B
+      | .rdiv L B A' Δ Λ =>
+          have h0: L ++ [B ⧸ A'] ++ Δ ++ Λ = Γ := sorry
+          have h1: list_degree Δ + tp_degree A' < list_degree Γ + tp_degree (# s) := by grind
+          have h2: list_degree (L ++ [B] ++ Λ) + tp_degree (# s) < list_degree Γ + tp_degree (# s) := by grind
+          prove Δ A' && prove (L ++ [B] ++ Λ) (# s)
+      | .ldiv Λ A' B Δ R =>
+          have h0: Λ ++ [A' ⧹ B] ++ Δ ++ R = Γ := sorry
+          have h1: list_degree Δ + tp_degree A' < list_degree Γ + tp_degree (# s) := by grind
+          have h2: list_degree (Λ ++ [B] ++ R) + tp_degree (# s) < list_degree Γ + tp_degree (# s) := by grind
+          prove Δ A' && prove (Λ ++ [B] ++ R) (# s))
+  | Tp.ldiv A' B =>
+    have h0: A = A' ⧹ B := sorry
+    have h1: list_degree ([A'] ++ Γ) + tp_degree B < list_degree Γ + tp_degree A := by grind
+    Γ ≠ [] && prove ([A'] ++ Γ) B
+  | Tp.rdiv B A' =>
+    have h0: A = B ⧸ A' := sorry
+    have h1: list_degree (Γ ++ [A']) + tp_degree B < list_degree Γ + tp_degree A := by grind
+    Γ ≠ [] && prove (Γ ++ [A']) B
 termination_by
   list_degree Γ + tp_degree A
 decreasing_by
-  all_goals
-    sorry
--- example : [Tp.atom "A" ⧸ Tp.atom "B", Tp.atom "B"] ⇒ (Tp.atom "A") := by grind
+  all_goals grind
+
 
 end Mathling.Lambek.ProductFree
