dcf0557f9d23ec5c3b99467c92620c7363de6af9
Shun Nakatani <shun_nktn@hotmail.com>
2026-05-05 14:29:12 +0900

    CCG.reduce(簡約のロジック)を追加

--- Mathling/CCG/Classical.lean
+++ Mathling/CCG/Classical.lean
@@ -8,4 +8,29 @@ inductive Reduce : List Category → List Category → Prop where
   | backward (Γ : List Category) (u v : Category) (Γ' : List Category) :
     Reduce (Γ ++ u :: (u \ v) :: Γ') (Γ ++ v :: Γ') 
 
+def reduceCore (l r : List Category) : List (List Category) :=
+  match r with
+  | [] => []
+  | [_] => []
+  | x₁ :: x₂ :: xs =>
+    let l' := l.concat x₁
+    let r' := x₂ :: xs
+    let recursiveResult := reduceCore l' r'
+    match x₁, x₂ with
+    | .forward v u, _ =>
+      if u == x₂ then
+        (l ++ v :: xs) :: recursiveResult
+      else
+        recursiveResult
+    | _, .backward u v =>
+      if u == x₁ then
+        (l ++ v :: xs) :: recursiveResult
+      else
+        recursiveResult
+    | _, _ =>
+      recursiveResult
+
+def reduce (cats : List Category) : List (List Category) :=
+  reduceCore [] cats
+
 end Mathling
